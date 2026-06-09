8403cd848064eb3dd1330f054128102c67b0707a
Drini30 <mateosamanta2@gmail.com>
2026-06-09 12:07:36 +0200

    Add files via upload

--- soccer_api.py
+++ soccer_api.py
@@ -39,7 +39,9 @@ def analizo_ndeshjen_premium(ekipi_1, ekipi_2, date_target):
     else: hint_id = 4
     
     rezultati_sakt = f"{gola_1}-{gola_2}"
-    koef_rez_sakt = round(random.uniform(6.50, 24.00), 2)
+    
+    # 🔥 KOEFICIENTI TANI FILLON NGA 7.00 PËR T'IU PËRSHTATUR ÇMIMEVE TUAJA TË REJA 🔥
+    koef_rez_sakt = round(random.uniform(7.00, 24.00), 2)
         
     return f"{koef_1:.2f}", f"{koef_x:.2f}", f"{koef_2:.2f}", parashikimi, hint_id, besueshmeria, rezultati_sakt, f"{koef_rez_sakt:.2f}"
 
